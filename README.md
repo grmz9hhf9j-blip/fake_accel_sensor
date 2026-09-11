5ee455e4f268bed4a78ffd36fbf73c25f4d93e8e
D.M. <grmz9hhf9j@privaterelay.appleid.com>
2026-09-11 12:41:39 +0200

    Adding sensor skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Temp title.
+
+This code is used to imitate a sensor.... (TODO: Fill in the README when the code is ready)
---
+++ include/sensor_xyz.h
@@ -0,0 +1,11 @@
+#ifndef SENSOR_XYZ_DRIVER_H
+#define SENSOR_XYZ_DRIVER_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/kernel.h>
+
+
+#endif // SENSOR_XYZ_DRIVER_H
