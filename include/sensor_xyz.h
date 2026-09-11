0c1554b542006e188ee5c5c93276348b9959df2c
D.M. <grmz9hhf9j@privaterelay.appleid.com>
2026-09-11 17:34:25 +0200

    latch sensro readings during fetching

--- include/sensor_xyz.h
+++ include/sensor_xyz.h
@@ -7,9 +7,15 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 
-struct sensor_xyz_data {
+struct sensor_xyz_sample {
 	struct sensor_value ax_ms2;
 	struct sensor_value ay_ms2;
+};
+
+struct sensor_xyz_data {
+	struct sensor_xyz_sample latest;
+	struct sensor_xyz_sample fetched;
+	bool fetched_valid;
 };
 
 #endif // SENSOR_XYZ_DRIVER_H
